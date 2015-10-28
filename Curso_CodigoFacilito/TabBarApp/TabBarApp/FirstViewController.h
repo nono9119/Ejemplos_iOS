55fbb6bfbf662abface02c49790912f475b39970
Nono <nono_20491@hotmail.com>
2015-10-28 15:00:48 +0100

    TabBarApp
    Aplicación para aprender a crear más pestañas en una misma app.

---
+++ Curso_CodigoFacilito/TabBarApp/TabBarApp/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TabBarApp
+//
+//  Created by Antonio Martínez González on 28/10/15.
+//  Copyright © 2015 Antonio Martínez González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Curso_CodigoFacilito/TabBarApp/TabBarApp/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  TabBarApp
+//
+//  Created by Antonio Martínez González on 28/10/15.
+//  Copyright © 2015 Antonio Martínez González. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
